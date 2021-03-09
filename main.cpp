0333fce093f89c9a0ddbd9c21ea6e483a3c5bb38
HHyeon <sunki1123@naver.com>
2021-03-10 00:36:06 +0900

    edit-2103101236

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <SFML/Graphics.hpp>
 using namespace std;
@@ -5,27 +6,29 @@ using namespace sf;
 
 int main()
 {
-   RenderWindow window(VideoMode(800,600),"highresimageviewer", Style::Default);
+   RenderWindow window(VideoMode(1600,900),"highresimageviewer", Style::Default);
    window.setVerticalSyncEnabled(true);
 
    Texture t1,t2;
    t1.loadFromFile("99.jpg");
-   t2.loadFromFile("tyuleniy_oli_2016097_lrg.jpg");
+//   t2.loadFromFile("tyuleniy_oli_2016097_lrg.jpg");
 
    cout << "w:" << t1.getSize().x << endl;
    cout << "h:" << t1.getSize().y << endl;
    Sprite s(t1);
+   s.setOrigin(t1.getSize().x/2-window.getSize().x,t1.getSize().y/2-window.getSize().y);
 
    int sx=0,sy=0;
    int dx=0,dy=0;
    int currx=0,curry=0;
    bool moving = false;
-   float zoomlevel=1.0, zoomlevel_past=0.0;
+   float zoomlevel=1.0, zoomlevel_past=1.0;
 
-   View view(FloatRect(0,0,800,600));
+   View view(FloatRect(0,0,window.getSize().x,window.getSize().y));
    view.setCenter(Vector2f(0,0));
    view.zoom(zoomlevel);
    window.setView(view);
+
 
    Event e;
    while(window.isOpen())
@@ -47,65 +50,46 @@ int main()
          {
             if(e.mouseButton.button == Mouse::Left)
             {
-               sx=Mouse::getPosition().x ;
-               sy=Mouse::getPosition().y;
                moving=true;
+               sx = Mouse::getPosition().x;
+               sy = Mouse::getPosition().y;
             }
          }
          else if(e.type == Event::MouseButtonReleased)
          {
             if(e.mouseButton.button == Mouse::Left)
             {
-               sx=sy=-1;
+               moving = false;
                currx = dx;
                curry = dy;
-               moving = false;
             }
          }
          else if(e.type == Event::MouseWheelMoved)
          {
-            if(e.mouseWheel.delta>0)
+            if(e.mouseWheel.delta<0)
             {
-               if(zoomlevel<3)
-                  zoomlevel++;
+               if(zoomlevel<5.0) zoomlevel*=1.5;
             }
             else
             {
-               if(zoomlevel>1)
-                  zoomlevel--;
+               if(zoomlevel>0.5) zoomlevel*=0.5;
             }
          }
       }
 
-      window.clear();
-
-      if(moving || (zoomlevel != zoomlevel_past))
+      if(moving)
       {
-         int pushx = (currx/zoomlevel);
-         int pushy = (curry/zoomlevel);
-
-         if(zoomlevel != zoomlevel_past)
-         {
-            pushx = pushy = 0;
-         }
-
-
-         dx = sx - Mouse::getPosition().x + pushx;
-         dy = sy - Mouse::getPosition().y + pushy;
-
-         cout << sx << ", " << sy << " - " << dx << ", " << dy << endl;
-
-         dx*=zoomlevel;
-         dy*=zoomlevel;
-
-         View v = window.getDefaultView();
-         v.zoom(zoomlevel);
-         v.move(Vector2f(dx,dy));
-         window.setView(v);
-
-         zoomlevel_past = zoomlevel;
+         dx = (sx - Mouse::getPosition().x)*zoomlevel + (currx);
+         dy = (sy - Mouse::getPosition().y)*zoomlevel + (curry);
       }
 
+//      cout << dx << ", " << dy << ", " << zoomlevel << endl;
+
+      window.clear();
+      View v = window.getDefaultView();
+      v.zoom(zoomlevel);
+      v.move(Vector2f(dx,dy));
+      window.setView(v);
       window.draw(s);
       window.display();
    }
